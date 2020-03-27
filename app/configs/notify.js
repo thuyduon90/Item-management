a45dd7df9bb3cc5730dfb24962cc02e38e37b039
ThuyDuong <thuydv1111@gmail.com>
2020-03-27 22:53:46 +0900

    first commit

---
+++ app/configs/notify.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    CHANGE_STATUS_SUCCESS: 'Changed status successfully!',
+    CHANGE_STATUS_MULTI_SUCCESS: 'There are %d items have been changed status successfully!',
+    CHANGE_ORDERING_SUCCESS: 'Changed ordering successfully!',
+    DELETE_SUCCESS: 'Deleted item successfully!',
+    DELETE_MULTI_SUCCESS: 'There are %d items have been deleted successfully!',
+    ADD_SUCCESS: 'Added successfully!',
+    EDIT_SUCCESS: 'Edited successfully',
+    ERROR_NAME: 'Length must be from %d to %d letters',
+    ERROR_ORDERING: 'Must be an interger greated than %d and less than %d',
+    ERROR_STATUS: 'Must be not empty!',
+};
